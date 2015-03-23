ba5268c8fefa864a1b5e44de3430f018843be6bb
if1live <libsora25@gmail.com>
2015-03-24 00:16:48 +0900

    tree 구조 변경. web+headless 컨트롤 조합으로 목표 변경. 프로토타입 작동 확인

--- mamiru/app.py
+++ mamiru/app.py
@@ -1,12 +1,57 @@
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+#!-*- coding: utf-8 -*-
 
-from .loggers import create_logger
-from .mpd_player import MPDPlayer
+from __future__ import print_function
+from __future__ import unicode_literals
 
-mpd_player = MPDPlayer()
-mpd_player.open()
+from asyncore import file_dispatcher, loop
+from evdev import InputDevice, categorize, ecodes
+from importd import d
+import thread
+import sys
 
-logger = create_logger('mamiru')
+class InputDeviceDispatcher(file_dispatcher):
+    def __init__(self, device):
+        self.device = device
+        file_dispatcher.__init__(self, device)
+
+    def recv(self, ign=None):
+        return self.device.read()
+
+    def handle_read(self):
+        for event in self.recv():
+            print(repr(event))
+
+d(
+    DEBUG=True,
+    INSTALLED_APPS=(
+        # django library
+        'django_nose',
+    ),
+    TEST_RUNNER='django_nose.NoseTestSuiteRunner'
+ )
 
 
+def run_device_input_thread():
+    print("device input thread start")
+    dev = InputDevice('/dev/input/event0')
+    try:
+        InputDeviceDispatcher(dev)
+        loop()
+    except KeyboardInterrupt:
+        print("quit")
 
+def run_web_server_thread():
+    #print("web server thread start")
+    d.main()
+    #print("web server thread stop")
+
+@d('/')
+def hello(request):
+    return d.HttpResponse('Hello world')
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        if sys.argv[1] in ('runserver',):
+            thread.start_new_thread(run_device_input_thread, ())
+    run_web_server_thread()
